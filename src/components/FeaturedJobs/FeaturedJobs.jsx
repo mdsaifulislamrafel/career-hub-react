33aa1729aa22485b606b75599eeda5a191394b83
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-03-23 01:54:59 +0600

    show all jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,7 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [dataLength, setDataLength] = useState(4);
     useEffect(() => {
         fetch('jobs.json')
             .then((res) => res.json())
@@ -14,10 +15,15 @@ const FeaturedJobs = () => {
                 <h2 className="text-5xl font-bold">Featured Jobs {jobs.length}</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 my-8">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <div className="text-center">
+                    <button onClick={() => setDataLength(jobs.length)} className="btn text-white font-bold bg-[#7E90FE]">Show All Jobs</button>
+                </div>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,8 +1,32 @@
 /* eslint-disable react/prop-types */
+import { CiLocationOn } from "react-icons/ci";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+
+
 const Job = ({ job }) => {
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
-        <div>
-            <h2>{job.job_title}</h2>
+        <div className="space-y-4 rounded-lg  p-6 border-2 bg-white">
+            <img alt="card navigate ui" src={logo} />
+            <div className="grid gap-2">
+                <h1 className="text-lg font-semibold ">{job_title}</h1>
+                <p className="text-sm text-gray-500 dark:text-gray-400">{company_name}</p>
+            </div>
+            <div className="flex gap-4">
+                <button className="px-4 py-2 bg-white text-[#7E90FE] font-bold hover:bg-gray-800 hover:text-white border-[#7E90FE] border duration-300 rounded-md">{remote_or_onsite}</button>
+                <button className="px-4 py-2 bg-white text-[#7E90FE] font-bold hover:bg-gray-800 hover:text-white border-[#7E90FE] border duration-300 rounded-md">{job_type}</button>
+            </div>
+            <div className="flex gap-10">
+                <div className="flex items-center gap-1">
+                    <CiLocationOn className="text-xl" />
+                    <p>{location}</p>
+                </div>
+                <div className="flex items-center gap-1">
+                    <RiMoneyDollarCircleLine className="text-xl" />
+                    <p>{salary}</p>
+                </div>
+            </div>
+            <button className="px-4 py-2 bg-[#7E90FE] hover:bg-gray-800 text-white font-bold duration-300 rounded-md">View Details</button>
         </div>
     );
 };
